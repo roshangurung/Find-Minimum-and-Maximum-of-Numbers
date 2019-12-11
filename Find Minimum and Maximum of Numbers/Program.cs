0251637cd81f2dbbd717ba2091f4a9d79cf7e23b
roshangurung <r.gurung1992@gmail.com>
2019-12-10 19:27:12 -0800

    Initial commit

---
+++ Find Minimum and Maximum of Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Find_Minimum_and_Maximum_of_Numbers
+{
+    class Program///This C# program is used to find minimum and maximum of numbers. Using array1 variable we have already defined the values, hence the minimum and the maximum number are obtained with the help of min and max functions.///
+    {
+        static void Main()
+        {
+            int[] array1 = { 10, -10, -20, 0, 15, 20, 30 };
+            Console.WriteLine("Maximum Element : " + array1.Max());
+            Console.WriteLine("Minimum Element : " + array1.Min());
+            Console.Read();
+        }
+    }
+}
+///Output:
+///Maximum Element:30
+///Minimum Element:-20
